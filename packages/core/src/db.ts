ac2a23970b4bf3c9e9ec961bbb5cb1d1a5dc4635
Alper Halil <aktasalper99@gmail.com>
2026-07-26 18:02:53 +0200

    enable WAL mode for databaes

--- packages/core/src/db.ts
+++ packages/core/src/db.ts
@@ -2,3 +2,5 @@ import { Database } from "bun:sqlite";
 import { DB_PATH } from "./constant";
 
 export const db = new Database(DB_PATH, { create: true });
+db.run("PRAGMA journal_mode = WAL;");
+// TODO: https://bun.com/docs/runtime/sqlite#wal-sidecar-file-cleanup macOS does not auto cleanup
