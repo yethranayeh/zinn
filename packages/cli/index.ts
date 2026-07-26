b8058f184c36b47b6ef11c5f19b7b77e2d2ccbb7
Alper Halil <aktasalper99@gmail.com>
2026-07-26 10:10:25 +0200

    add tty check for empty argument launch

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,10 +1,12 @@
-// Main entry point, will replace the code currently published on https://www.npmjs.com/package/@zinn-dev/cli
-
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  const tui = await import("@zinn-dev/tui");
-  tui.launch();
+  if (!!process.stdout.isTTY) {
+    const tui = await import("@zinn-dev/tui");
+    tui.launch();
+  } else {
+    // TODO: non interative branch
+  }
 } else {
   // TODO: headless CLI
 }
